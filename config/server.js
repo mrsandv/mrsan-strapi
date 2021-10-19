2f6d66dc5d9db695bec02c37468f94681581c223
mrsan <marcoantoniosandovalespinosa@Marco-Antonio-Sandoval-Espinosa.local>
2021-10-18 19:31:31 -0500

    project configs

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eb9163cb31a2bd42429f537a47ee8dca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '80ebc109-0fb3-49db-a2a9-b29b85a399a2'
+};
